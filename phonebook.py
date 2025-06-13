613396aa38104877e44e4522e80e2729153c78a3
Maurício Augusto <78917687+mrc-augusto@users.noreply.github.com>
2025-06-13 16:30:48 -0300

    initial commit

---
+++ phonebook.py
@@ -0,0 +1,15 @@
+contactsList=[]
+
+while True:
+  print('\nMenu Agenda de Contatos')
+  print('1. Adicionar Contato')
+  print('2.Visualizar Lista de Contatos')
+  print('3. Editar Contato')
+  print('4. Marcar/Desmarcar contato como favorito')
+  print('5. Ver Lista de Favoritos')
+  print('6. Apagar Contato')
+  print('7. Sair da Agenda')
+
+  choice = input('Escolha uma opção: ')
+
+  
